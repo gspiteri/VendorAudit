991039af3c2b40b26f757e4f9a6fdac72ca23ae2
Grant Spiteri <gdspiteri@live.com>
2017-05-16 22:13:30 +1000

    added nuget restore and some autofac settings

---
+++ VendorAuditTracker.Webai/App_Start/AutofacConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using VendorAuditTracker.Webapi.Services;
+
+namespace VendorAuditTracker.Webapi
+{
+    public class AutofacConfig
+    {
+        public static void RegisterComponents()
+        {
+            var builder = new ContainerBuilder();
+
+            // Get your HttpConfiguration.
+            var config = GlobalConfiguration.Configuration;
+
+            // Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            // OPTIONAL: Register the Autofac filter provider.
+            builder.RegisterWebApiFilterProvider(config);
+
+            // Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            builder.RegisterType<VendorService>().As<IVendorService>().InstancePerRequest();
+            builder.RegisterType<ReleaseService>().As<IReleaseService>().InstancePerRequest();
+        }
+    }
+}
--- VendorAuditTracker.Webai/Services/IReleaseService.cs
+++ VendorAuditTracker.Webai/Services/IReleaseService.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace VendorAuditTracker.Webapi.Services
 {
-    public class ReleaseService
+    public interface IReleaseService
     {
     }
 }
