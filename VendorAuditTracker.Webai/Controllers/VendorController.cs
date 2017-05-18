87107cec4467d78e8eb07e1be1583f53a969dabf
Grant Spiteri <gdspiteri@live.com>
2017-05-18 22:58:44 +1000

    add missing files

---
+++ VendorAuditTracker.Webai/Controllers/VendorController.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Results;
+using VendorAuditTracker.Webapi.DataTransferObjects.Response;
+using VendorAuditTracker.Webapi.Services;
+
+namespace VendorAuditTracker.Webapi.Controllers
+{
+    public class VendorController : BaseController
+    {
+        private readonly IVendorService _vendorService;
+
+
+        public VendorController(IVendorService vendorService)
+        {
+            _vendorService = vendorService;
+        }
+
+        [Route("api/Vendor")]
+        [HttpGet]
+        public virtual async Task<IHttpActionResult> Get()
+        {
+            HttpStatusCode status = HttpStatusCode.Accepted;
+            VendorResponse response = new VendorResponse();
+            try
+            {
+                response = await _vendorService.GetAll();
+                status = response.IsSuccessful && response.Errors.Count == 0 ? HttpStatusCode.OK : HttpStatusCode.BadRequest;
+                return ResponseMessage(Request.CreateResponse(HttpStatusCode.OK, response));
+            }
+            catch (Exception)
+            {
+
+            }
+            return new NegotiatedContentResult<VendorResponse>(status, response, this);
+        }
+    }
+}
